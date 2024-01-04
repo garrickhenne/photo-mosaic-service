b35cb501ec9509bbeb200f827b8360f3e6df04b3
Garrick Henne <garrickhenne@outlook.com>
2024-01-03 19:04:33 -0800

    Implement RGBAPixel class

---
+++ RGBAPixel.py
@@ -0,0 +1,52 @@
+from ctypes.wintypes import RGB
+
+
+class RGBAPixel:
+  def __init__(self) -> None:
+    self.r = 0
+    self.g = 0
+    self.b = 0
+    self.a = 1.0
+  
+  def __init__(self, r, g, b) -> None:
+    self.r = r
+    self.g = g
+    self.b = b
+    self.a = 1.0
+
+  def __init__(self, r, g, b, a) -> None:
+    self.r = r
+    self.g = g
+    self.b = b
+    self.a = a
+
+  def __eq__(self, __value: 'RGBAPixel') -> bool:
+    if (self.a == __value.a):
+      return True
+    if (abs(self.r - __value.r > 2)):
+      return False
+    if (abs(self.g - __value.g > 2)):
+      return False
+    if (abs(self.b - __value.b > 2)):
+      return False
+    
+    return True
+
+  def __ne__(self, __value: 'RGBAPixel') -> bool:
+    return not self.__eq__(__value)
+  
+  def __lt__(self, __value: 'RGBAPixel') -> bool:
+    if (self == __value):
+      return False
+    
+    if (self.r < __value.r):
+      return True
+    elif ((self.r == __value.r) and (self.g < __value.g)):
+      return True
+    elif ((self.r == __value.r) and (self.g == __value.g) and (self.b < __value.b)):
+      return True
+    
+    return False
+  
+  def __str__(self) -> str:
+    return f'({self.r}, {self.g}, {self.b}, {self.a})'
